071734520155ff0d7db4ba7e612b74957b0630dd
xanderzeng <xanderzeng@gmail.com>
2018-03-18 20:16:48 +0800

    login process added

--- test.js
+++ test.js
@@ -6,13 +6,22 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use(express.static(__dirname +'/front-end/login&signup/'));
 
-app.get('/front-end/tasty-master/home.html',function(request, response){
+app.get('/front-end/login&signup/index.html',function(request, response){
 	response.sendFile(__dirname + "/front-end/login&signup/" + 'index.html');
 })
 
 app.post('/register_process', urlencodedParser, function(req,res){
 	var response = {
 		"name":req.body.name,
+		"email":req.body.email,
+		"password":req.body.password
+	};
+	/***Some operations to verify if the account is eligeble*/
+	res.end(JSON.stringify(response));
+})
+
+app.post('/login_process', urlencodedParser, function(req,res){
+	var response = {
 		"email":req.body.email,
 		"password":req.body.password
 	};
